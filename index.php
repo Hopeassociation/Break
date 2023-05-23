dd1e97de486764c24bf26659de7e65240510aaee
Emmanuellensossani <emmanuelle.nsossani@etu.u-pec.fr>
2023-05-23 09:35:45 +0200

    arborescence

--- index.php
+++ index.php
@@ -1,16 +1,48 @@
-<section class="main">
-    <a href="#"><h1>Grenada</h1></a>
-    
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="../../../public/css/index.css">
+  <title>titre</title>
+</head>
+
+<body>
+
+  <header class="header">
+    <nav id="menu">
+      <a href="../../../break">
+        <img src="../../../public/images/logo_break.png" alt="lego_break">
+      </a>
+      <ul>
+        <li><a href="#">Nos bières</a></li>
+        <li><a href="#">Fabrication</a></li>
+        <li><a href="#">Nos engagements</a></li>
+      </ul>
+      <button type="button"><a href="">Acheter ici</a></button>
+    </nav>
+  </header>
+  <section class="main">
+    <a href="#">
+      <h1>Grenada</h1>
+    </a>
+
     <div class="swatch">
-        <div id="b1"><a href="https://www.mauiultrafins.fr/aileron/aileron-freeride/"><img src="public\images\coco.png" alt="biere_coco"></div></a>
-        <div><img src="public\images\passion.png" alt="biere_passion"></div>
-        <div><img src="public\images\litchi.png" alt="biere_passion"></div>
-        <div><img src="public\images\ananas.png" alt="biere_passion"></div>
-        <div><img src="public\images\hibiscus.png" alt="biere_passion"></div>
-        <div><img src="public\images\grenade.png" alt="biere_passion"></div>
-      </div>
-    
-    </section>
+      <div id="b1"><a href="https://www.mauiultrafins.fr/aileron/aileron-freeride/"><img src="public\images\coco.png" alt="biere_coco"></div></a>
+      <div><img src="public\images\passion.png" alt="biere_passion"></div>
+      <div><img src="public\images\litchi.png" alt="biere_passion"></div>
+      <div><img src="public\images\ananas.png" alt="biere_passion"></div>
+      <div><img src="public\images\hibiscus.png" alt="biere_passion"></div>
+      <div><img src="public\images\grenade.png" alt="biere_passion"></div>
+    </div>
+
+  </section>
 
 </body>
+
 </html>
