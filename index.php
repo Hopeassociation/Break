9e081ba4e525b8ba7dfee15fc16b61df636e3425
Emmanuellensossani <emmanuelle.nsossani@etu.u-pec.fr>
2023-05-24 20:56:34 +0200

    grenada + biere

--- index.php
+++ index.php
@@ -5,14 +5,13 @@
     <h1>Grenada</h1>
   </div>
 
-
   <div class="swatch">
-    <div id="b1"><a href="https://www.mauiultrafins.fr/aileron/aileron-freeride/"><img src="C4 copie.png"></div></a>
-    <div><img src="P6 copie.png"></div>
-    <div><img src="L1 copie.png"></div>
-    <div><img src="A5 copie.png"></div>
-    <div><img src="h2 copie.png"></div>
-    <div><img src="G1 copie.png"></div>
+    <div id="b1"><a href="https://www.mauiultrafins.fr/aileron/aileron-freeride/"><img alt="coco" src="public/images/mockup/coco.png"></div></a>
+    <div><img alt="passion" src="public/images/mockup/passion.png"></div>
+    <div><img alt="litchi" src="public/images/mockup/litchi.png"></div>
+    <div><img alt="ananas" src="public/images/mockup/ananas.png"></div>
+    <div><img alt="hibiscus" src="public/images/mockup/hibiscus.png"></div>
+    <div><img alt="grenada" src="public/images/mockup/grenada.png"></div>
   </div>
 
 </section>
