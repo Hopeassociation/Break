44e98324f4da8ef8265c828318b08d95b9958786
Emmanuellensossani <emmanuelle.nsossani@etu.u-pec.fr>
2023-05-25 00:31:32 +0200

    index php fini

--- app/view/parts/acceuil.view.php
+++ app/view/parts/acceuil.view.php
@@ -5,12 +5,12 @@
   </div>
 
   <div class="swatch">
-    <div><img alt="coco" src="public/images/mockup/coco.png"></div></a>
-    <div><img alt="passion" src="public/images/mockup/passion.png"></div>
-    <div><img alt="litchi" src="public/images/mockup/litchi.png"></div>
-    <div><img alt="ananas" src="public/images/mockup/ananas.png"></div>
-    <div><img alt="hibiscus" src="public/images/mockup/hibiscus.png"></div>
-    <div><img alt="grenada" src="public/images/mockup/grenada.png"></div>
+  <div><img alt="coco" src=" <?= $biere_photo["coco"]["photo"] ?> "></div>
+    <div><img alt="passion" src=" <?= $biere_photo["passion"]["photo"] ?> "></div>
+    <div><img alt="litchi" src=" <?= $biere_photo["litchi"]["photo"] ?> "></div>
+    <div><img alt="ananas" src=" <?= $biere_photo["ananas"]["photo"] ?> "></div>
+    <div><img alt="hibiscus" src=" <?= $biere_photo["hibiscus"]["photo"] ?> "></div>
+    <div><img alt="grenada" src=" <?= $biere_photo["grenada"]["photo"] ?> "></div>
   </div>
   <div id="grid">
     <img src="public/images/grid_biere.svg" alt="grid">
