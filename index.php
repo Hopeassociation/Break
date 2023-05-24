748bdf2a7ee7be54193dbd40672d5b2b8f577a1f
Emmanuellensossani <emmanuelle.nsossani@etu.u-pec.fr>
2023-05-24 21:49:12 +0200

    ajout grille

--- index.php
+++ index.php
@@ -6,12 +6,15 @@
   </div>
 
   <div class="swatch">
-    <div id="b1"><a href="https://www.mauiultrafins.fr/aileron/aileron-freeride/"><img alt="coco" src="public/images/mockup/coco.png"></div></a>
+    <div><img alt="coco" src="public/images/mockup/coco.png"></div></a>
     <div><img alt="passion" src="public/images/mockup/passion.png"></div>
     <div><img alt="litchi" src="public/images/mockup/litchi.png"></div>
     <div><img alt="ananas" src="public/images/mockup/ananas.png"></div>
     <div><img alt="hibiscus" src="public/images/mockup/hibiscus.png"></div>
     <div><img alt="grenada" src="public/images/mockup/grenada.png"></div>
+  </div>
+  <div id="grid">
+    <img src="public/images/grid_biere.svg" alt="grid">
   </div>
 
 </section>
