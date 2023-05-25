3a65d548d46b3dae931bd029606ebe6e442fd101
Emmanuellensossani <emmanuelle.nsossani@etu.u-pec.fr>
2023-05-25 22:56:49 +0200

    ...

--- app/view/parts/nosbieres.view.php
+++ app/view/parts/nosbieres.view.php
@@ -4,16 +4,10 @@
         <h2>vous devriez aimez ceci !</h2>
     </div>
     <section id="biere">
+        
         <div class="biere">
-            <h3>Grenada</h3>
-                    
+            <h3>Grenada</h3>                
         </div>
-        <div class="biere">
-            <img src="encadre.svg" alt="">
-        </div>
-        <div class="biere"></div>
-        <div class="biere"></div>
-        <div class="biere"></div>
-        <div class="biere"></div>
+        
     </section>
 </main>
